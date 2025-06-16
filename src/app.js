d58f2183312145a5caa4eddd57f5fc2319c5ed76
Pedro Valencia Alves <pedrovalenciaalves98@gmail.com>
2025-06-16 20:09:02 +0000

    lineamiento de codigo

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ let noun = ['jogger', 'racoon'];
 let extensions = ['.com', '.org', '.net'];
 
 
-for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-            for (let l = 0; l < extensions.length; l++) {
-                console.log(pronoun[i] + adj[j] + noun[k] + extensions[l]);
-            }
-        }
-    }
-}
+pronoun.forEach(p => {
+    adj.forEach(a => {
+        noun.forEach(n => {
+            extensions.forEach(e => {
+                console.log(p+a+n+e)
+            })
+        })
+    })
+})
